b84d7a82bb2b3813900d86538e1682c32bce955e
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-07-16 07:59:02 -0400

    added functionality to "Upload to your story" page

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -22,8 +22,43 @@ class UploadController extends AbstractController
     /**
      * upload-submit
      */
-    public function submit()
+    public function submit(Request $request, EntityManagerInterface $entityManager)
     {
+        $file = $request->files->get('file');
+        $name = $request->request->get('name');
+        $caption = $request->request->get('caption');
+        $date = $request->request->get('date');
+        $tz = $request->request->get('tz');
+        $location = $request->request->get('location');
+        $tags = $request->request->get('tags');
+        $message = $request->request->get('message');
+
+        if ($file === null || $name === null || $caption === null || $date === null || $location === null || $tags === null || $message === null) {
+            throw new \Exception("Missing fields: need to POST 'file', 'name', 'caption', 'date', 'location', 'tags', 'message'; received " . print_r($request->request->all(), true));
+        }
+
+        $upload = new Entity\Upload;
+
+        if ($file->isValid()) {
+            $path = '/var/www/html/public/uploads/';
+            $fn = $file->getClientOriginalName();
+            if (file_exists($path . $fn)) {
+                $fn = uniqid() . '.' . $file->guessClientExtension();
+            }
+            $file->move($path, $fn);
+            $upload->setFile($fn);
+        }
+
+        $upload->setName($name);
+        $upload->setCaption($caption);
+        $upload->setDate(new DateTime($date, new DateTimeZone($tz)));
+        $upload->setLocation($location);
+        $upload->setTags($tags);
+        $upload->setMessage($message);
+
+        $entityManager->persist($upload);
+        $entityManager->flush();
+
         return new JsonResponse(true);
     }
 }
---
+++ src/Entity/Upload.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UploadRepository")
+ */
+class Upload
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $caption;
+
+    /**
+     * @ORM\Column(type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tags;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCaption(): ?string
+    {
+        return $this->caption;
+    }
+
+    public function setCaption(string $caption): self
+    {
+        $this->caption = $caption;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getTags(): ?string
+    {
+        return $this->tags;
+    }
+
+    public function setTags(string $tags): self
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}

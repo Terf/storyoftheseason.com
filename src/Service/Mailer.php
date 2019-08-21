114bc2509d42b575813a2054ff1828455e04c3af
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-20 23:10:10 -0400

    removed all email styling

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -12,92 +12,9 @@ class Mailer
         <!DOCTYPE html>
         <html>
           <head>
-          <style>
-        @media only screen and (min-width: 0) {
-          .wrapper {
-            text-rendering: optimizeLegibility;
-          }
-        }
-        @media only screen and (max-width: 620px) {
-          [class=wrapper] {
-            min-width: 302px !important;
-            width: 100% !important;
-          }
-        
-          [class=wrapper] .block {
-            display: block !important;
-          }
-        
-          [class=wrapper] .hide {
-            display: none !important;
-          }
-        
-          [class=wrapper] .top-panel,
-        [class=wrapper] .header,
-        [class=wrapper] .main,
-        [class=wrapper] .footer {
-            width: 302px !important;
-          }
-        
-          [class=wrapper] .title,
-        [class=wrapper] .subject,
-        [class=wrapper] .signature,
-        [class=wrapper] .subscription {
-            display: block;
-            float: left;
-            width: 300px !important;
-            text-align: center !important;
-          }
-        
-          [class=wrapper] .signature {
-            padding-bottom: 0 !important;
-          }
-        
-          [class=wrapper] .subscription {
-            padding-top: 0 !important;
-          }
-        }
-        </style>
           </head>
-          <body style="margin: 0; padding: 0; mso-line-height-rule: exactly; min-width: 100%; background-color: #ffffff;">
-            <center class="wrapper" style="display: table; table-layout: fixed; width: 100%; min-width: 620px; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; background-color: #ffffff;">
-                <table class="top-panel center" width="602" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border-spacing: 0; margin: 0 auto; width: 602px;">
-                    <tbody>
-                    <tr>
-                        <td class="title" width="300" style="vertical-align: top; text-align: left; width: 300px; padding: 8px 0; color: #616161; font-family: Roboto, Helvetica, sans-serif; font-weight: 400; font-size: 12px; line-height: 14px;" valign="top" align="left">Story of the Season</td>
-                        <td class="subject" width="300" style="vertical-align: top; text-align: right; width: 300px; padding: 8px 0; color: #616161; font-family: Roboto, Helvetica, sans-serif; font-weight: 400; font-size: 12px; line-height: 14px;" valign="top" align="right"><a class="strong" href="https://storyoftheseason.com" target="_blank" style="text-decoration: none; color: #616161; font-weight: 700;">storyoftheseason.com</a></td>
-                    </tr>
-                    <tr>
-                        <td class="border" colspan="2" style="padding: 0; vertical-align: top; font-size: 1px; line-height: 1px; background-color: #e0e0e0; width: 1px;" width="1" valign="top" bgcolor="#e0e0e0">&nbsp;</td>
-                    </tr>
-                    </tbody>
-                </table>
-        
-                <div class="spacer" style="font-size: 1px; width: 100%; line-height: 16px;">&nbsp;</div>
-        
-                <table class="main center" width="602" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border-spacing: 0; -webkit-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24); -moz-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24); box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24); margin: 0 auto; width: 602px;">
-                    <tbody>
-                    <tr>
-                        <td class="column" style="padding: 0; vertical-align: top; text-align: left; background-color: #ffffff; font-size: 14px;" valign="top" align="left" bgcolor="#ffffff">
-                            <div class="column-top" style="font-size: 24px; line-height: 24px;">&nbsp;</div>
-                            <table class="content" border="0" cellspacing="0" cellpadding="0" style="border-collapse: collapse; border-spacing: 0; width: 100%;" width="100%">
-                                <tbody>
-                                <tr>
-                                    <td class="padded" style="vertical-align: top; padding: 0 24px;" valign="top">
-                                      '.$message.'
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                            <div class="column-bottom" style="font-size: 8px; line-height: 8px;">&nbsp;</div>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-        
-                <div class="spacer" style="font-size: 1px; width: 100%; line-height: 16px;">&nbsp;</div>
-
-            </center>
+          <body style="margin: 0; padding: 10px; mso-line-height-rule: exactly; min-width: 100%; background-color: #ffffff;color: #616161; font-family: Roboto, Helvetica, sans-serif; font-weight: 400; font-size: 12px; line-height: 14px;"">
+            '.$message.'
           </body>
         </html>';
 

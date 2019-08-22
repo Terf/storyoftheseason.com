8542ac9044e0ab7a331e9ca07a44804f35314370
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-08-22 17:10:31 -0400

    temporary code to address that some user accounts didnt get created on kitaboos end because kitaboo didnt delete test users from their database but we did from ours, so user accounts w low ID #s didnt get created in kitaboo because the test users existed in there

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -32,6 +32,7 @@ class LoginController extends AbstractController
             return new JsonResponse(['result' => false, 'reason' => 'email']);
         } else {
             if (password_verify($password, $user->getPassword())) {
+                $user = $this->ensureKitabooRegistration($user, $password, $entityManager);
                 $token = bin2hex(random_bytes(16));
                 $user->setToken($token);
                 $entityManager->merge($user);
@@ -47,5 +48,30 @@ class LoginController extends AbstractController
         $response = $this->redirectToRoute('index');
         $response->headers->clearCookie('user_token');
         return $response;
+    }
+
+    private function ensureKitabooRegistration($user, $unhashedPassword, $entityManager)
+    {
+        // these users didnt have their accounts created correctly on Kitaboo's end, if they log in again make sure to create their account
+        $corruptedAccounts = ['content@storyoftheseason.com', 'downingchris33@gmail.com', 'omartabdoun@gmail.com', 'rachaelristas@roadrunner.com', 'theo@planetkipp.com', 'gypsyheiress@gmail.com', 'sharon217@comcast.net', 'vricken95@gmail.com', 'ejwssse@gma', 'kbinkowski35@yahoo.com', 'jr.ogle@abcglobal.net'];
+        if (in_array($user->getEmail(), $corruptedAccounts)) {
+            $newUser = clone $user;
+            $entityManager->persist($newUser);
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $data = json_encode([
+                'user' => [
+                    'firstName' => $newUser->getFirstName(),
+                    'lastName' => $newUser->getLastName(),
+                    'userName' => $newUser->getEmail(),
+                    'password' => $unhashedPassword,
+                    'clientUserID' => $newUser->getId(),
+                    'email' => $newUser->getEmail()
+                ]
+            ]);
+            shell_exec('sudo docker run --rm -e ACTION=register_user -e DATA='.escapeshellarg($data).' -e CONSUMER_KEY='.getenv('KITABOO_CONSUMER_KEY_PROD').' -e SECRET_KEY='.getenv('KITABOO_SECRET_KEY_PROD').' --name running-kitaboo kitaboo');
+            return $newUser;
+        }
+        return $user;
     }
 }
